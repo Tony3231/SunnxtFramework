047e56db3fb369d29b6fdb41f196b1fb8aa83004
Tony3231 <142726590+Tony3231@users.noreply.github.com>
2025-08-26 14:28:51 +0530

    Update baseFunction.java

--- src/main/java/sunnxtPages/baseFunction.java
+++ src/main/java/sunnxtPages/baseFunction.java
@@ -17,101 +17,89 @@ import com.aventstack.extentreports.ExtentTest;
 import utility.ExtentReportManager;
 import utility.Log;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger; // << Needed!
+import org.apache.logging.log4j.Logger;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class baseFunction {
 
-	public WebDriver driver;
-	protected static ExtentReports extent;
-	protected ExtentTest test;
+    public WebDriver driver;
+    protected static ExtentReports extent;
+    protected ExtentTest test;
     private static final Logger log = Log.getLogger(baseFunction.class);
 
     @BeforeSuite
-	public void setupReport() {
-		extent = ExtentReportManager.getReportInstance();
-	}
-	
-	@AfterSuite
-	public void teardownReport() {
-		extent.flush();
-		//String reportPath = ExtentReportManager.reportPath;
-		//EmailUtils.sendTestReport(reportPath);
-	}
-    
-    
-    
-	public void setupDriver() {
-
-
-    // Setup ChromeDriver automatically
-    WebDriverManager.chromedriver().setup();
-
-    // Configure ChromeOptions
-    ChromeOptions options = new ChromeOptions();
-    Map<String, Object> prefs = new HashMap<>();
-    prefs.put("profile.default_content_setting_values.notifications", 2);
-    options.setExperimentalOption("prefs", prefs);
-
-    options.addArguments("--no-sandbox");
-    options.addArguments("--disable-dev-shm-usage");
-    options.addArguments("--disable-gpu");
-    options.addArguments("--remote-allow-origins=*");
-    options.addArguments("--user-data-dir=/tmp/chrome-" + System.currentTimeMillis());
-
-    // Add headless mode for non-Windows environments
-    String os = System.getProperty("os.name").toLowerCase();
-    if (!os.contains("win")) {
-        options.addArguments("--headless=new"); // Modern headless mode
+    public void setupReport() {
+        extent = ExtentReportManager.getReportInstance();
     }
 
-    // Instantiate WebDriver
-    driver = new ChromeDriver(options);
-    driver.manage().window().maximize();
-    System.out.println("WebDriver initiated and Chrome launched using WebDriverManager");
-}	
-	}
+    @AfterSuite
+    public void teardownReport() {
+        extent.flush();
+        // String reportPath = ExtentReportManager.reportPath;
+        // EmailUtils.sendTestReport(reportPath);
+    }
 
-	public void launchSunnxt() {
-        log.info("Launching Sun NXT.....");   		
-		driver.get("https://www.sunnxt.com/");
-		
-	}
+    public void setupDriver() {
+        // Setup ChromeDriver automatically
+        WebDriverManager.chromedriver().setup();
 
-	public void quitDriver() {
-		implicitWait(30);
-		driver.quit();
-		log.info("All browser sessions have been closed.");
+        // Configure ChromeOptions
+        ChromeOptions options = new ChromeOptions();
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("profile.default_content_setting_values.notifications", 2);
+        options.setExperimentalOption("prefs", prefs);
 
-	}
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--user-data-dir=/tmp/chrome-" + System.currentTimeMillis());
 
-	public void closeSession() {
-		driver.close();
-		log.info("Current session has been closed.");
-	}
+        // Add headless mode for non-Windows environments
+        String os = System.getProperty("os.name").toLowerCase();
+        if (!os.contains("win")) {
+            options.addArguments("--headless=new"); // Modern headless mode
+        }
 
-	public void implicitWait(int seconds) {
-		driver.manage().timeouts().implicitlyWait(seconds, TimeUnit.SECONDS);
-       log.info("Applying implicit wait of {} seconds.", seconds);
+        // Instantiate WebDriver
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        System.out.println("WebDriver initiated and Chrome launched using WebDriverManager");
+    }
 
-	}
+    public void launchSunnxt() {
+        log.info("Launching Sun NXT...");
+        driver.get("https://www.sunnxt.com/");
+    }
 
-	public String randomEmail() {
+    public void quitDriver() {
+        implicitWait(30);
+        driver.quit();
+        log.info("All browser sessions have been closed.");
+    }
 
-		Random random =new Random();
-		String randomEmailId = "asgkhbkjk"+Integer.toString(random.nextInt(100))+"@hotfail.com";
-		log.debug("Generated random email: {}", randomEmailId);
-		return randomEmailId;
+    public void closeSession() {
+        driver.close();
+        log.info("Current session has been closed.");
+    }
 
-	}
+    public void implicitWait(int seconds) {
+        driver.manage().timeouts().implicitlyWait(seconds, TimeUnit.SECONDS);
+        log.info("Applying implicit wait of {} seconds.", seconds);
+    }
 
-	public void notificationBlocker() {
+    public String randomEmail() {
+        Random random = new Random();
+        String randomEmailId = "asgkhbkjk" + random.nextInt(100) + "@hotfail.com";
+        log.debug("Generated random email: {}", randomEmailId);
+        return randomEmailId;
+    }
+
+    public void notificationBlocker() {
         log.info("Setting Chrome options to block notifications.");
-		ChromeOptions options=new ChromeOptions();
-		Map<String, Object> prefs=new HashMap<>();
-		prefs.put("profile.default_content_setting_values.notifications", 2);
-		options.setExperimentalOption("prefs", prefs);
-	}
-
+        ChromeOptions options = new ChromeOptions();
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("profile.default_content_setting_values.notifications", 2);
+        options.setExperimentalOption("prefs", prefs);
+    }
 }
